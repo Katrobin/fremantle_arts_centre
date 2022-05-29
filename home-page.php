de49514bd0fd4a3e50ed36af5960596143e96857
Katrobin <100463891+Katrobin@users.noreply.github.com>
2022-05-29 10:43:58 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,108 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header();?>
+<section class="container-fluid bg2 intro"> <!-- introduction section -->
+      <div class="container pad20">
+        <div class="row whattext">
+      <h2 class="blue">What makes concerts at Fremantle Arts Centre so special?</h2>
+    </div>
+    <div class = "container">
+      
+      </div> <!-- container -->
+      
+
+      
+      </section> <!-- introduction section -->
+
+<section class="container-fluid bg3">
+  <div class="container">
+  <div class="row justify-content-center featured-events" id="parallelogram">
+    <h2 class="bannerstyle">FEATURED EVENTS</h2>
+    </div>
+
+<div class="row pad30">
+
+<div class="col-md-6">
+    <h3>OCEAN ALLEY</h3>
+    <div class="bio" id="bioshapeleft">Hailing from Sydney's Northern Beaches, Ocean Alley have solidified their position as one of Australia's most exciting bands. Having surpassed 200 million catalogue streams.</div>
+    <div class="pad15">
+    <a class="btn btn-sm btn-stylebold" href="#">Buy tickets</a></div>
+</div>
+
+<div class="col-md-6">
+    <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub1/wp-content/themes/fremantle_arts_centre/images/oceanalley.jpg" alt="4 members of Ocean Alley standing in a row">
+
+</div>
+</div> <!-- row -->
+
+<div class="row pad30">
+  
+  <div class="col-md-6">
+      <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub1/wp-content/themes/fremantle_arts_centre/images/leon.jpg" alt="Leon Bridges sitting in a car">
+    </div>
+    <div class="col-md-6 text-right">
+      <h3>LEON BRIDGES</h3>
+      <div class="bio" id="bioshaperight">Leon Bridges returns to Aussie stages with The Boundless Tour. The Grammy Award-winning R&B artist and songwriter brings to the stage a career spanning experiences, inspirations, and award-winning catalogue of songs.</div>
+      <div class="pad15">
+      <a class="btn btn-sm btn-stylebold" href="#">Buy tickets</a></div>
+  </div>
+  
+  </div> <!-- row -->
+
+
+  <div class="row bannersocial justify-content-center">
+    <img class= padright src="http://206.189.45.97/~mesh21/sub1/wp-content/themes/fremantle_arts_centre/images/jon.png" alt="picture of John Roberts social media profile">
+ <span class="strong pad30">@johnrobertslive</span>
+<span class="pad30">...Had the best time at Fremantle Arts Centre last night. Spacey Jane were on fire!</span>
+  </div>
+
+
+<div class="row pad30">
+  <div class="col-md-6">
+      <h3>L.A.B.</h3>
+      <div class="bio" id="bioshapeleft1">The L.A.B sound is one which has seen them quickly become a fan favourite on the New Zealand live circuit - trademark Kora grooves combining with Adams-Tamatea's reggae/dub experience.</div>
+      <div class="pad15">
+      <a class="btn btn-sm btn-stylebold" href="#">Buy tickets</a></div>
+  
+  </div> <!-- colmd5 -->
+  
+  <div class="col-md-6">
+      <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub1/wp-content/themes/fremantle_arts_centre/images/lab.jpg" alt="5 members of L.A.B. sitting in a row">
+  </div>
+  </div> <!-- row -->
+  
+  <div class="row pad30">
+    <div class="col-md-6">
+        <img class="img-fluid" src="http://206.189.45.97/~mesh21/sub1/wp-content/themes/fremantle_arts_centre/images/timrogers.jpg" alt="Tim Rogers in a hat">
+      </div>
+      <div class="col-md-6 text-right">
+        <h3>TIM ROGERS AND FRIENDS</h3>
+        <div class="bio" id="bioshaperight1">You Am I frontman Tim Rogers goes solo...with some friends. Expect an evening of pure entertainment and this master Australian songwriter takes you on a journey through his inner most workings.</div>
+        <div class="pad15">
+        <a class="btn btn-sm btn-stylebold" href="#">Buy tickets</a></div>
+    </div> <!-- row -->
+    </div>
+    
+  
+
+    <div class="row bannersocial justify-content-center">
+      <img class= padright src="http://206.189.45.97/~mesh21/sub1/wp-content/themes/fremantle_arts_centre/images/penny.png" alt="picture of PennyLovesToParty social media profile">
+   
+   <span class="strong pad30">@pennylovestoparty</span>
+    <span class="pad30">...The Fremantle Arts Centre lawn for concerts is EVERYTHING. I'll be back to check out Ocean Alley in a few weeks time!</span>
+   </div>
+  
+    <div class="col-md-1"></div>
+
+    <div class="row eventslink pad30">
+<a href="events.html">See more events coming up at Fremantle Arts Centre</a>
+    </div>
+
+  </div> <!-- container featured events-->
+
+
+    </section>
+    <?php get_footer();?>
